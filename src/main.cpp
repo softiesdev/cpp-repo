e15c5cb1f0646ada91dc19db2c27e3f5fd1e7e12
Maxime Verreault <maximeverreault24@hey.com>
2024-02-19 17:42:32 -0500

    Added basic tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
+#include "module1.hpp"
 #include <iostream>
 
 int main() {
-  std::cout << "Hello world!" << std::endl;
+  std::cout << "Hello world! " << add2(2) << std::endl;
   return 0;
 }
---
+++ src/module1.cpp
@@ -0,0 +1,2 @@
+#include "module1.hpp"
+int add2(int i) { return i + 2; }
